8dd3a819a37c5af83e8d6138457a06154c9fbdb4
WatchList <130539461+watch-list@users.noreply.github.com>
2025-12-09 17:15:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ const allPrompts = [
         image: "Gemini_Generated_Image_om4ivxom4ivxom4i.png",
         prompt: "Isometric view of a cozy magic potion shop, low poly 3d art, purple and orange color palette, glowing bottles, tiny details, cute atmosphere.",
         category: "midjourney"
-    }
+    },
     {
         id: 4,
         image: "https://i.postimg.cc/59srh3ms/Gemini-Generated-Image-31ovid31ovid31ov.png",
         prompt: "Danielle from NewJeans, standing casually in a tranquil snowy landscape, wearing relaxed  Perfect lighting highlights Danielle's features, ensuring she harmonizes with the peaceful natural environment, creating an inviting and warm atmosphere amidst the cold. 4:5.",
         category: "nanobanana"  // <--- ADD THIS
-    },
+    }
 ];
